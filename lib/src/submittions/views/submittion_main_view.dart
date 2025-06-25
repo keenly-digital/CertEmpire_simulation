7228e27e8191a6c8288cc137aeb150e9d8290b27
Zohaib Khalid <zohaibkhalid95@gmail.com>
2025-06-25 14:57:36 +0500

    minor

--- lib/src/submittions/views/submittion_main_view.dart
+++ lib/src/submittions/views/submittion_main_view.dart
@@ -7,7 +7,7 @@ class SubmittionMainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.center,
+      alignment: Alignment.topCenter,
       child: Image.asset(Assets.submittion),
     );
   }
