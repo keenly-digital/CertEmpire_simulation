3d89d8372be9ec33a18334681d0a47a9fbdc6ee3
Zohaib Khalid <zohaibkhalid95@gmail.com>
2025-06-25 14:25:02 +0500

    UI Updates

--- lib/core/shared/widgets/header.dart
+++ lib/core/shared/widgets/header.dart
@@ -6,23 +6,23 @@ import '../../config/theme/app_colors.dart';
 Widget header() {
   return // Scrollable header
   Container(
-    height: 70.h,
-    margin: EdgeInsets.only(top: 20),
+    height: 80.h,
     color: AppColors.themeBlue,
     padding: EdgeInsets.symmetric(horizontal: 16.w),
     child: Row(
       children: [
-        Image.asset('assets/images/CertEmpire_Logo.png', height: 50.h, width: 150.h),
+        Image.asset(
+          'assets/images/CertEmpire_Logo.png',
+          height: 50.h,
+          width: 150.h,
+        ),
         const Spacer(),
         ElevatedButton.icon(
           onPressed: () {},
           icon: const Icon(Icons.arrow_back_rounded, color: AppColors.black),
           label: const Text(
             'Back to main website',
-            style: TextStyle(
-              color: AppColors.black,
-              // fontWeight: FontWeight.bold,
-            ),
+            style: TextStyle(color: AppColors.black),
           ),
           style: ElevatedButton.styleFrom(
             backgroundColor: Color(0xFFF7E5FF),
