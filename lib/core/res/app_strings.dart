303ebde6977401c051c6cd41533737c9f59d6980
Zohaib Khalid <zohaibkhalid95@gmail.com>
2025-07-06 16:55:53 +0500

    Changes

--- lib/core/res/app_strings.dart
+++ lib/core/res/app_strings.dart
@@ -22,16 +22,16 @@ class AppStrings {
   static const String close = 'CLOSE';
   static const String reportAnswerAsIncorrect = 'Report Answer as Incorrect';
 
-  // static String fileId = '';
-  // static String authToken = "";
-  // static int id = 0;
-  // static String userId = '';
-  static String fileId = 'f9699361-dfd0-4336-83c0-8eccb5b8b472';
-  static int orderId = 0;
+  static String fileId = '';
   static String authToken = "";
-  static int id = 10860;
-  static String userId = '97cf017a-32c0-4b2e-934a-aa9680e9d544';
-  static String baseUrl = "https://staging2.certempire.com";
+  static int id = 0;
+  static String userId = '';
 
+  // static String fileId = 'f9699361-dfd0-4336-83c0-8eccb5b8b472';
+  // static int id = 10860;
+  // static String userId = '97cf017a-32c0-4b2e-934a-aa9680e9d544';
+
+  static String baseUrl = "https://staging2.certempire.com";
   static String name = "Ali R Zain";
+  static int orderId = 0;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,7 @@ class _AppBootstrapperState extends State<AppBootstrapper> {
         AppStrings.id = data['id'] ?? '10860';
         AppStrings.userId = data['simulation_user_id'];
         AppStrings.authToken = encodedData;
+        AppStrings.name = data['name'] ?? '';
       } catch (e) {
         debugPrint("Error decoding data: $e");
       }
